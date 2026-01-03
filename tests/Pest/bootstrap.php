3b69e77f30d701583fdb936d7687fef9284f7209
Rasso Hilber <mail@rassohilber.com>
2026-01-03 19:46:46 +0100

    Fix paths in Pest bootstrap file

--- tests/Pest/bootstrap.php
+++ tests/Pest/bootstrap.php
@@ -2,32 +2,27 @@
 
 namespace Hirasso\ACFFF\Tests\Pest;
 
-/**
- * wp-phpunit bootstrap file
- *
- * @see https://github.com/wp-phpunit/example-plugin/blob/master/tests/bootstrap.php
- */
+/** The wordpress plugins directory, deducted from this bootstrap file */
+$pluginsDir = \dirname(\dirname(\dirname(__DIR__)));
 
-// Load wp-env's config file in the container, but still use our own wp-phpunit
+/** Load wp-env's config file in the container, but still use our own wp-phpunit */
 \putenv('WP_PHPUNIT__TESTS_CONFIG=/wordpress-phpunit/wp-tests-config.php');
 
-// Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
-require_once \dirname(\dirname(__DIR__)) . '/vendor/autoload.php';
+/** Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available */
+require_once "$pluginsDir/acf-frontend-forms/vendor/autoload.php";
 
-// Give access to tests_add_filter() function.
+/** Give access to tests_add_filter() function */
 require_once \getenv('WP_PHPUNIT__DIR') . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
-\tests_add_filter('muplugins_loaded', function () {
+/** Manually load the plugin being tested. */
+\tests_add_filter('muplugins_loaded', function () use ($pluginsDir) {
     // require ACF, which is a dependency of ACFML
-    require_once(\dirname(\dirname(__DIR__)) . '/advanced-custom-fields/acf.php');
+    require_once("$pluginsDir/advanced-custom-fields/acf.php");
     // require the main plugin file
-    require_once(\dirname(__DIR__) . '/acf-frontend-forms.php');
+    require_once("$pluginsDir/acf-frontend-forms/acf-frontend-forms.php");
     // don't autamatically load acfml in tests
     \remove_action('plugins_loaded', 'acfff');
 });
 
-// Start up the WP testing environment.
+/** Start up the WP testing environment. */
 require_once \getenv('WP_PHPUNIT__DIR') . '/includes/bootstrap.php';
